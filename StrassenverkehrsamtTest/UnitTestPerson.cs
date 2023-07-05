3b9bc65dc47e6a9a8263b0bcb71c970620126483
ahmad.gholamsakhi <ahmad.gholamsakhi@GHOL-PC>
2023-07-05 11:50:02 +0200

    Projektdateien hinzufügen.

---
+++ StrassenverkehrsamtTest/UnitTestPerson.cs
@@ -0,0 +1,15 @@
+using Ueb11a_Strassenverkehrsamt;
+
+namespace StrassenverkehrsamtTest
+{
+    [TestClass]
+    public class UnitTestPerson
+    {
+        [TestMethod]
+        public void TestMethode_Konstruktor()
+        {
+            Person p = new Person();
+            Assert.IsTrue(true);
+        }
+    }
+}
